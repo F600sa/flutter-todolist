c2030377af568a599b5ac58e283c0341d93b0b39
Alwaleed Fares <110339523+waleed00439@users.noreply.github.com>
2022-11-06 17:26:39 +0300

    Update taskController.dart

---
+++ todolist/lib/C/taskController.dart
@@ -0,0 +1,8 @@
+ import 'package:get/get_state_manager/get_state_manager.dart';
+
+// class ControllerTask extends GetxController {
+//   Future<void> removeTodo(ControllerTask task) async {
+//     await _datastoreService.removeTodo(task);
+//     tasks.remove(task);
+//   }
+// }
